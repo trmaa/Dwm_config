54d30eb1f2a70e2fda6adde320d311255364101c
trmaa <trmaayt@gmail.com>
2024-09-11 22:39:04 +0200

    displays cmd

--- commands/multiscreen.sh
+++ commands/multiscreen.sh
@@ -1,2 +1,1 @@
-# not working xrandr --output HDMI-2 --auto --left-of DP-1 --mode 1920x1080 
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output HDMI-2 --auto --same-as DP-1 --mode 1920x1080
